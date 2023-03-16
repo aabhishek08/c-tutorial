0f1b5d34e940b92a36c58a0ec1c9e25bc14a3d1a
aabhishek08 <126218455+aabhishek08@users.noreply.github.com>
2023-03-16 23:55:28 +0530

    Delete Question3a2.java

--- Question3a2.java
+++
@@ -1,3 +0,0 @@
-/*2) Write a program to find average of the numbers available in left and right side of the number
-required by user if available in an array of 10 user defined number otherwise replace the item of
-the array placed in middle position with the number required by user and calculate output?*/
